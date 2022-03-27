6bdf0ac3a61e8380c115a414a9845b0d6edcf897
Laura <arq.lauracordeiro@gmail.com>
2022-03-27 09:26:26 -0300

    configurando modal

---
+++ js/index.js
@@ -0,0 +1,10 @@
+reservar = () => {
+  let modal = document.querySelector(".modal");
+
+  modal.style.display = "block";
+};
+
+cancelar = () => {
+  let modal = document.querySelector(".modal");
+  modal.style.display = "block";
+};
