80ae966c92ebf5f487830d7cab0a3a5192652014
Laura Neves <lauratneves@gmail.com>
2022-03-28 08:28:48 -0300

    funcionalidade da página de Ver Reservas

--- js/reservas.js
+++ js/reservas.js
@@ -1,6 +1,9 @@
-// let id = window.location.href.split("=")[1];
+let id = window.location.href.split("=")[1];
 const BASE_URL_RESERVA = "https://xp41-soundgarden-api.herokuapp.com/bookings/event/";
 const BASE_URL_EVENTO = "https://xp41-soundgarden-api.herokuapp.com/events/";
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
 let table = document.querySelector("#table");
 let nomeEvento = document.getElementById("nome-evento");
@@ -8,31 +11,28 @@ let lotacaoEvento = document.getElementById("lotacao-evento");
 let reservasEvento = document.getElementById("reservas-evento");
 
 
-let idEvento = "623bc253d2891ac70ab1cce4";
-let idReserva = "623bcf860275f4bc2ca08cd3";
-
-
 async function LotacaoMax() {
-    const respostaLotacao = await fetch(`${BASE_URL_EVENTO}${idEvento}`, { method: "GET", redirect: "follow" });
+    const respostaLotacao = await fetch(`${BASE_URL_EVENTO}${id}`, { method: "GET", redirect: "follow" });
     const maxJson = await respostaLotacao.json();
     
-    console.log(maxJson);
-    // nomeEvento.innerHTML = resJson.name;
-    // lotacaoEvento.innerHTML = resJson.number_tickets;
+    nomeEvento.innerHTML = maxJson.name;
+    lotacaoEvento.innerHTML = maxJson.number_tickets;
 }
 LotacaoMax()
 
 
 async function Reservas() {
-    const respostaReserva = await fetch(`${BASE_URL_RESERVA}${idReserva}`, { method: "GET", redirect: "follow" });
+    const respostaReserva = await fetch(`${BASE_URL_RESERVA}${id}`, { method: "GET",
+    headers: myHeaders,
+    redirect: "follow" });
     const resJson = await respostaReserva.json();
     var contador = 0;
-
+    
     resJson.forEach((item, index) => {
         contador += item.number_tickets;
 
         table.innerHTML +=
-            `<tr >
+            `<tr>
             <th scope="row">${index + 1}</th>
             <td>${item.owner_name}</td>
             <td>${item.owner_email}</td>
