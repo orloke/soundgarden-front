6ea2773db6e9ed4d77340ef5728ea8d61e00a71e
Júnior Dering <juniordering@hotmail.com>
2022-03-29 19:44:15 -0400

    pequenas alterações modal

--- js/index.js
+++ js/index.js
@@ -6,11 +6,9 @@ let card = document.querySelector(".pagina_inicial");
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com/events";
 
 
-abrirModal.forEach(btn => {
-  btn.addEventListener("click", e => {
+var reservar = () =>{
     modal.style.display = "block";
-  });
-});
+}
 
 concluirReserva.addEventListener("click", e => {
   modal.style.display = "none";
@@ -29,7 +27,7 @@ var Listar = async () => {
       <h2 id="evento1">${item.name} - ${item.scheduled}</h2>
       <h4>${item.attractions}</h4>
       <p class="p_card_index">${item.description}</p>
-      <button class="btn btn-primary botao-reservar">
+      <button onclick ="reservar()" class="btn btn-primary botao-reservar">
         reservar ingresso
       </button>
     </article>`;
