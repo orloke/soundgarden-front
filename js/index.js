0673b045734d7e9795bae36a0be11a60f638c49f
Júnior Dering <juniordering@hotmail.com>
2022-03-29 18:25:39 -0400

    modificando index

--- js/index.js
+++ js/index.js
@@ -17,15 +17,18 @@ concluirReserva.addEventListener("mousedown", e => {
 var Listar = async () => {
   const resposta = await fetch(BASE_URL, { method: "GET" });
   const resJson = await resposta.json();
-  resJson.forEach(item => {
-    card.innerHTML += `<article class="cards_index evento card p-5 m-3">
-        <h2 id="evento1">${item.name} - ${item.scheduled}</h2>
-        <h4>${item.attractions}</h4>
-        <p class="p_card_index">${item.description}</p>
-        <button onclick="reservar()" class="btn btn-primary botao-reservar">
-          reservar ingresso
-        </button>
-      </article>`;
+  resJson.forEach((item,index) => {
+    if(index<2){
+      card.innerHTML += `<article class="cards_index evento card p-5 m-3">
+      <h2 id="evento1">${item.name} - ${item.scheduled}</h2>
+      <h4>${item.attractions}</h4>
+      <p class="p_card_index">${item.description}</p>
+      <button onclick="reservar()" class="btn btn-primary botao-reservar">
+        reservar ingresso
+      </button>
+    </article>`;
+    }
+
   });
 };
 
