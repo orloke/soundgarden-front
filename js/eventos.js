078b884650dbc05c92258418fb817d82d13fc224
Laura Neves <lauratneves@gmail.com>
2022-03-29 23:07:34 -0300

    implementação api no modal do index e eventos

--- js/eventos.js
+++ js/eventos.js
@@ -9,8 +9,6 @@ let atracoesEvento = document.getElementById("atracoes-evento");
 
 let card = document.querySelector(".pagina_inicial");
 
-let tituloBanner = document.getElementById("nome-evento-banner");
-
 let form = document.querySelector('form');
 let nome = document.querySelector('#nome');
 let sexo = document.querySelector('#sexo');
@@ -43,8 +41,7 @@ var Listar = async () => {
   const resposta = await fetch(BASE_URL, { method: "GET" });
   const resJson = await resposta.json();
   resJson.forEach((item,index) => {
-    if(index<4){
-      card.innerHTML += `<article class="cards_index evento card p-5 m-3">
+    card.innerHTML += `<article class="cards_index evento card p-5 m-3">
       <h2 id="evento${index+1}">${item.name} - ${item.scheduled}</h2>
       <h4>${item.attractions}</h4>
       <p class="p_card_index">${item.description}</p>
@@ -52,46 +49,9 @@ var Listar = async () => {
         reservar ingresso
       </button>
     </article>`;
-    }
-  });
- 
-imgBanner1();
-};
+    });
+}
 Listar();
-
-
-function imgBanner3(){
-  let img3 = document.getElementById("section-banner");
-  img3.style.background = "url('../img/festa.jpg') no-repeat center";
-  img3.style.backgroundSize = "cover";
-
-  let tituloEvento3 = document.getElementById("evento3").innerHTML;
-  tituloBanner.innerHTML = tituloEvento3;
-
-  setTimeout("imgBanner1()", 5000)
-}
-
-function imgBanner2(){
-  let img2 = document.getElementById("section-banner");
-  img2.style.background = "url('../img/samba.jpg') no-repeat center";
-  img2.style.backgroundSize = "cover";
-  
-  let tituloEvento2 = document.getElementById("evento2").innerHTML;
-  tituloBanner.innerHTML = tituloEvento2;
-  
-  setTimeout("imgBanner3()", 5000);
-}
-
-function imgBanner1(){
-  let img1 = document.getElementById("section-banner");
-  img1.style.background = "url('../img/rock-nacional.jpg') no-repeat center";
-  img1.style.backgroundSize = "cover";
-
-  let tituloEvento1 = document.getElementById("evento1").innerHTML;
-  tituloBanner.innerHTML = tituloEvento1;
-
-  setTimeout("imgBanner2()", 5000);
-}
 
 
 form.onsubmit = async (e)=>{
