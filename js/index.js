45d0e7dba5176ce9b7cafdf508817d0c1ff6a976
Júnior Dering <juniordering@hotmail.com>
2022-03-29 21:02:27 -0400

    tirando eventos pre-definidos

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ const abrirModal = document.querySelectorAll(".botao-reservar");
 const modal = document.getElementById("modal");
 const concluirReserva = document.querySelector(".concluir");
 const cancelarReserva = document.querySelector(".fechar");
+const descricao = document.querySelector(".qualquer");
 let card = document.querySelector(".pagina_inicial");
+let formodal = document.querySelector('.form-modal')
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com/events";
 
-
-var reservar = () =>{
-    modal.style.display = "block";
+var reservar = (a) =>{
+  modal.style.display = "block"; 
 }
 
 concluirReserva.addEventListener("click", e => {
@@ -22,18 +23,20 @@ var Listar = async () => {
   const resposta = await fetch(BASE_URL, { method: "GET" });
   const resJson = await resposta.json();
   resJson.forEach((item,index) => {
-    if(index<2){
+    if(index<4){
       card.innerHTML += `<article class="cards_index evento card p-5 m-3">
       <h2 id="evento1">${item.name} - ${item.scheduled}</h2>
       <h4>${item.attractions}</h4>
       <p class="p_card_index">${item.description}</p>
-      <button onclick ="reservar()" class="btn btn-primary botao-reservar">
+      <button onclick ="reservar('${item._id}')" class="btn btn-primary botao-reservar">
         reservar ingresso
       </button>
-    </article>`;
+    </article>`;    
     }
-
   });
 };
 
-Listar();
+Listar()
+
+
+
