7317220ef9f875b7a6654ddb8f9e4dc213858b55
Júnior Dering <juniordering@hotmail.com>
2022-03-30 22:02:46 -0400

    tentativa banner

--- js/index.js
+++ js/index.js
@@ -106,7 +106,7 @@ function imgBanner3(){
 
 function imgBanner2(){
   let img2 = document.getElementById("section-banner");
-  img2.style.background = "url('../img/samba.jpg') no-repeat center";
+  img2.style.background = "url('./img/samba.jpg') no-repeat center";
   img2.style.backgroundSize = "cover";
   
   let tituloEvento2 = document.getElementById("evento2").innerHTML;
@@ -117,7 +117,7 @@ function imgBanner2(){
 
 function imgBanner1(){
   let img1 = document.getElementById("section-banner");
-  img1.style.background = "url('../img/rock-nacional.jpg') no-repeat center";
+  img1.style.background = "url('./img/rock-nacional.jpg') no-repeat center";
   img1.style.backgroundSize = "cover";
 
   let tituloEvento1 = document.getElementById("evento1").innerHTML;
